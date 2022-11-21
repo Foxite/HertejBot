8832e0f280281817790fbe1deed6c9c86f451f73
Dirk Kok <the@dirkkok.nl>
2022-11-21 15:09:19 +0100

    wip back button for rating

--- HertejBot/HertejDB/RateService.cs
+++ HertejBot/HertejDB/RateService.cs
@@ -8,7 +8,8 @@ using Microsoft.Extensions.Options;
 namespace HertejBot.HertejDB; 
 
 public class RateService {
-	private static readonly Regex InteractionIdParser = new Regex(@"^rate-(?<userid>\d+)-(?<imageid>\d+)-(?<rating>yes|no)-(?<category>.+)?$");
+	private static readonly Regex InteractionIdParser_Submit = new Regex(@"^rate-(?<imageid>\d+)-(?<rating>yes|no)-(?<category>.+)?$");
+	private static readonly Regex InteractionIdParser_Back = new Regex(@"^back-(?<imageid>\d+)-(?<category>.+)?$");
 	
 	private readonly HertejClient m_Hertej;
 	private readonly IOptionsMonitor<HertejClient.Options> m_HertejOptions;
@@ -24,11 +25,17 @@ public class RateService {
 			.WithImageUrl($"{m_HertejOptions.CurrentValue.BaseUrl}/Image/{image.Id}/download");
 	}
 
-	private DiscordComponent[] GetComponents(string userId, string? category, GetImageDto image) {
-		return new DiscordComponent[] {
+	private IEnumerable<DiscordComponent> GetComponents(ulong userId, string? category, GetImageDto image, string? previousId) {
+		IEnumerable<DiscordComponent> ret = new DiscordComponent[] {
 			new DiscordButtonComponent(ButtonStyle.Success, $"rate-{userId}-{image.Id}-yes-{category}", "Yes", emoji: new DiscordComponentEmoji(DiscordEmoji.FromUnicode("👍"))),
-			new DiscordButtonComponent(ButtonStyle.Danger, $"rate-{userId}-{image.Id}-no-{category}", "No", emoji: new DiscordComponentEmoji(DiscordEmoji.FromUnicode("👎")))
+			new DiscordButtonComponent(ButtonStyle.Danger, $"rate-{userId}-{image.Id}-no-{category}", "No", emoji: new DiscordComponentEmoji(DiscordEmoji.FromUnicode("👎"))),
 		};
+
+		if (previousId != null) {
+			ret = ret.Append(new DiscordButtonComponent(ButtonStyle.Danger, $"back-{previousId}-{category}", "No", emoji: new DiscordComponentEmoji(DiscordEmoji.FromUnicode("👎"))));
+		}
+
+		return ret;
 	}
 
 	public async Task<DiscordFollowupMessageBuilder> GetUnratedImage(ulong userId, string? category) {
@@ -41,13 +48,13 @@ public class RateService {
 		} else {
 			return builder
 				.AddEmbed(GetEmbed(image))
-				.AddComponents(GetComponents(userId.ToString(), category, image))
+				.AddComponents(GetComponents(userId, category, image, null))
 				.AsEphemeral();
 		}
 	}
 
-	private async Task<DiscordWebhookBuilder> GetNextUnratedImage(string userId, string? category) {
-		GetImageDto? image = await m_Hertej.GetUnratedImage(userId, category);
+	private async Task<DiscordWebhookBuilder> GetNextUnratedImage(ulong userId, string? category, string? previousId) {
+		GetImageDto? image = await m_Hertej.GetUnratedImage(userId.ToString(), category);
 
 		var builder = new DiscordWebhookBuilder();
 		
@@ -56,22 +63,41 @@ public class RateService {
 		} else {
 			return builder
 				.AddEmbed(GetEmbed(image))
-				.AddComponents(GetComponents(userId, category, image));
+				.AddComponents(GetComponents(userId, category, image, previousId));
 		}
 	}
 
+	private async Task<DiscordWebhookBuilder> GetPreviousUnratedImage(ulong userId, string? category, string previousId) {
+		GetImageDto image = await m_Hertej.GetImage(previousId);
+
+		return new DiscordWebhookBuilder()
+			.AddEmbed(GetEmbed(image))
+			.AddComponents(GetComponents(userId, category, image, null));
+	}
+
 	public async Task<bool> HandleRatingInteraction(ComponentInteractionCreateEventArgs args) {
-		Match match = InteractionIdParser.Match(args.Id);
+		Match match = InteractionIdParser_Submit.Match(args.Id);
 		if (match.Success) {
 			await args.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
 			
-			await m_Hertej.SubmitRating(match.Groups["imageid"].Value, match.Groups["userid"].Value, match.Groups["rating"].Value == "yes");
+			await m_Hertej.SubmitRating(match.Groups["imageid"].Value, args.User.Id.ToString(), match.Groups["rating"].Value == "yes");
 
-			await args.Interaction.EditFollowupMessageAsync(args.Message.Id, await GetNextUnratedImage(match.Groups["userid"].Value, match.Groups["category"].Success ? match.Groups["category"].Value : null));
+			DiscordWebhookBuilder messageBuilder = await GetNextUnratedImage(args.User.Id, match.Groups["category"].Success ? match.Groups["category"].Value : null, match.Groups["imageid"].Value);
+			await args.Interaction.EditFollowupMessageAsync(args.Message.Id, messageBuilder);
 			
 			return true;
-		} else {
-			return false;
 		}
+		
+		match = InteractionIdParser_Back.Match(args.Id);
+		if (match.Success) {
+			await args.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+
+			DiscordWebhookBuilder messageBuilder = await GetPreviousUnratedImage(args.User.Id, match.Groups["category"].Success ? match.Groups["category"].Value : null, match.Groups["imageid"].Value);
+			await args.Interaction.EditFollowupMessageAsync(args.Message.Id, messageBuilder);
+			
+			return true;
+		}
+		
+		return false;
 	}
 }
